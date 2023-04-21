29396a100152f7dfa73637874a3b1520e1016754
saeeddawod <saeed.dawod@gmail.com>
2023-04-21 13:19:48 +0300

    bank loan task

---
+++ course2-part1/bank-loan-task/loan-task.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.4;
+
+contract BankSystem {
+    // Define structs for Account, Loan, AccountType, and LoanType
+    
+    // Define mappings for accounts, loans, account types, and loan types
+    
+    // Define events for important actions
+    
+    // Define modifiers for access control
+    
+    // Implement account management functions (including interest calculation and fees)
+    
+    // Implement loan management functions (including interest calculation and fees)
+    
+    // Implement bank policy and configuration functions
+}
