8bd55374a9a040b40e882dbb0d06384a70740ba5
SaeeDawod <122533407+SaeeDawod@users.noreply.github.com>
2023-06-12 20:35:42 +0300

    Update custom-erc20-task.sol

--- course2-part1/erc20-timeLocking/custom-erc20-task.sol
+++ course2-part1/erc20-timeLocking/custom-erc20-task.sol
@@ -5,33 +5,19 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
-        // TO DO: Mint the initial supply to msg.sender.
-        // Hint: Use the _mint() function provided by the ERC20 contract.
-        // The _mint() function requires two parameters: the recipient of the tokens and the amount.
+    address public owner;
+    mapping(address => bool) public whitelist;
+
+    constructor() ERC20("MyToken", "MTK") {
+        owner = msg.sender;
+        // TO DO: Mint 10 tokens to this contract.
     }
 
     // TO DO: Implement the transfer function.
-    // This function should allow a user to transfer a specified amount of tokens to another user.
-    // Hint: Use the _transfer() function provided by the ERC20 contract.
-    // The _transfer() function requires three parameters: the sender, the recipient, and the amount.
-    function transferTokens(address recipient, uint256 amount) public {
-        // your code here
-    }
-
-    // TO DO: Implement the burn function.
-    // This function should allow a user to burn (destroy) a specified amount of their own tokens.
-    // Hint: Use the _burn() function provided by the ERC20 contract.
-    // The _burn() function requires two parameters: the burner's address and the amount to burn.
-    function burnTokens(uint256 amount) public {
-        // your code here
-    }
 
     // TO DO: Implement the burnFrom function.
-    // This function should allow an approved spender to burn a specified amount of tokens on behalf of the token owner.
-    // Hint: Use the _burn() function provided by the ERC20 contract.
-    // The _burn() function requires two parameters: the owner's address and the amount to burn.
-    function burnFromTokens(address account, uint256 amount) public {
-        // your code here
-    }
+
+    // TO DO: Implement the addToWhitelist function.
+
+    // TO DO: Implement the withdrawToken function.
 }
