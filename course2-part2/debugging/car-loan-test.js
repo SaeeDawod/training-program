6673bb4a64527ff95413cad94a49a999dfc3f588
saeeddawod <saeed.dawod@gmail.com>
2023-06-19 23:32:44 +0300

    new car rental tests

--- course2-part2/debugging/car-loan-test.js
+++ course2-part2/debugging/car-loan-test.js
@@ -1,60 +1,41 @@
-const { expect } = require("chai");
+import { ethers } from "hardhat";
+import chai from "chai";
+import { solidity } from "ethereum-waffle";
+import { CarRental } from "../typechain/CarRental";
 
-describe("CarLoan", function() {
-    let carLoan;
-    let owner;
-    let borrower;
+chai.use(solidity);
+const { expect } = chai;
 
-    beforeEach(async function() {
-      const CarLoan = await ethers.getContractFactory("CarLoan");
-      [owner, borrower, _] = await ethers.getSigners();
-      carLoan = await CarLoan.connect(owner).deploy(ethers.utils.parseEther("1000000"), 5);
-    });
+describe("Car Rental", function () {
+  let carRental: CarRental;
 
-    describe("requestLoan", function() {
-        it("Should successfully request a loan", async function() {
-            await carLoan.connect(borrower).requestLoan(ethers.utils.parseEther("1"), ethers.utils.parseEther("2"));
-            const loan = await carLoan.loans(borrower.address);
-            expect(loan.amount).to.equal(ethers.utils.parseEther("1"));
-            expect(loan.interestRate).to.equal(5);
-            expect(loan.collateral).to.equal(ethers.utils.parseEther("2"));
-            expect(loan.approved).to.be.false;
-            expect(loan.repaid).to.be.false;
-        });
+  beforeEach(async function () {
+    // Deploying contract before each test
+    const CarRentalFactory = await ethers.getContractFactory("CarRental");
+    carRental = (await CarRentalFactory.deploy()) as CarRental;
+    await carRental.deployed();
+  });
 
-        it("Should not allow to request another loan before repaying the current one", async function() {
-            await carLoan.connect(borrower).requestLoan(ethers.utils.parseEther("1"), ethers.utils.parseEther("2"));
-            await expect(carLoan.connect(borrower).requestLoan(ethers.utils.parseEther("1"), ethers.utils.parseEther("2"))).to.be.revertedWith("Existing loan must be repaid first.");
-        });
-    });
+  it("Should add a new car", async function () {
+    await carRental.addCar("Test Car", ethers.utils.parseEther("1"));
+    const car = await carRental.cars(0);
+    expect(car.name).to.equal("Test Car");
+    expect(car.deposit).to.equal(ethers.utils.parseEther("1"));
+    expect(car.rented).to.equal(false);
+  });
 
-    describe("approveLoan", function() {
-        it("Should successfully approve a loan", async function() {
-            await carLoan.connect(borrower).requestLoan(ethers.utils.parseEther("1"), ethers.utils.parseEther("2"));
-            await carLoan.connect(owner).approveLoan(borrower.address);
-            const loan = await carLoan.loans(borrower.address);
-            expect(loan.approved).to.be.true;
-        });
-    });
+  it("Should rent a car", async function () {
+    await carRental.addCar("Test Car", ethers.utils.parseEther("1"));
+    await carRental.rentCar(0, 86400, { value: ethers.utils.parseEther("1") }); // Renting for 1 day
+    const car = await carRental.cars(0);
+    expect(car.rented).to.equal(true);
+  });
 
-    describe("repayLoan", function() {
-        it("Should successfully repay a loan", async function() {
-            await carLoan.connect(borrower).requestLoan(ethers.utils.parseEther("1"), ethers.utils.parseEther("2"));
-            await carLoan.connect(owner).approveLoan(borrower.address);
-            await carLoan.connect(borrower).repayLoan({ value: ethers.utils.parseEther("1.05") });
-            const loan = await carLoan.loans(borrower.address);
-            expect(loan.repaid).to.be.true;
-        });
-
-        it("Should not allow to repay an unapproved loan", async function() {
-            await carLoan.connect(borrower).requestLoan(ethers.utils.parseEther("1"), ethers.utils.parseEther("2"));
-            await expect(carLoan.connect(borrower).repayLoan({ value: ethers.utils.parseEther("1.05") })).to.be.revertedWith("Loan not approved.");
-        });
-
-        it("Should not allow to repay with incorrect amount", async function() {
-            await carLoan.connect(borrower).requestLoan(ethers.utils.parseEther("1"), ethers.utils.parseEther("2"));
-            await carLoan.connect(owner).approveLoan(borrower.address);
-            await expect(carLoan.connect(borrower).repayLoan({ value: ethers.utils.parseEther("1") })).to.be.revertedWith("Incorrect repayment amount.");
-        });
-    });
+  it("Should return a car", async function () {
+    await carRental.addCar("Test Car", ethers.utils.parseEther("1"));
+    await carRental.rentCar(0, 86400, { value: ethers.utils.parseEther("1") }); // Renting for 1 day
+    await carRental.returnCar({ value: ethers.utils.parseEther("0") }); // No delay, so no penalty
+    const car = await carRental.cars(0);
+    expect(car.rented).to.equal(false);
+  });
 });
