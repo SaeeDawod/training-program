05a4c9cf4c81b51ac8c76005e825f33cee94deae
SaeeDawod <122533407+SaeeDawod@users.noreply.github.com>
2023-07-03 15:05:45 +0300

    Update ether.js

--- course2-part2/interacting-with-blockchains-ethers-web3js-jsonrpc/ether.js
+++ course2-part2/interacting-with-blockchains-ethers-web3js-jsonrpc/ether.js
@@ -1,7 +1,7 @@
 //sending transaction with ethers
 import { JsonRpcProvider, utils } from 'ethers';
 
-const provider = new JsonRpcProvider('https://settlemint.infura.io/v3/YOUR-PROJECT-ID');
+const provider = new JsonRpcProvider('https://settlemint.com-url/api-key');
 const signer = provider.getSigner();
 const transaction = {
   to: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
@@ -14,7 +14,7 @@ console.log('Transaction Hash:', tx.hash);
 import { JsonRpcProvider, Contract } from 'ethers';
 import contractABI from './contractABI.json';
 
-const provider = new JsonRpcProvider('https://settlemint.infura.io/v3/YOUR-PROJECT-ID');
+const provider = new JsonRpcProvider('https://settlemint.com-url/api-key');
 const contractAddress = '0x9c02a4000b8Feb2b48D896B0DA64456b9B3a676D';
 const contract = new Contract(contractAddress, contractABI, provider.getSigner());
 
